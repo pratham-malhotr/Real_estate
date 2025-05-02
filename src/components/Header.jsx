6edbd0fa9071a9cc99e8d17f92223f13bda141d1
pratham-malhotr <prathammalhotra2410@gmail.com>
2025-05-03 00:34:23 +0530

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <span>Market</span>Tracker
+      </div>
+      <nav className="nav">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/services">Services</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
